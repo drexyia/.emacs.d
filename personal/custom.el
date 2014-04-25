7d35bc0a69ce68b8d9c6d0dff3155f4cc5494a40
drexyia <drexyia@fastmail.fm>
2014-04-25 11:50:26 +0100

    Update custom.el
    modified to include key chords

--- personal/custom.el
+++ personal/custom.el
@@ -1,22 +1,7 @@
-(defun drexyia-emacsd-install-packages ()
-  "installs all packages not installed as part of emacs prelude"
-  (let 
-      (packages-list  current-package)
-    
-    ;; all packages not installed within the prelude package
-    (setq packages-list '(helm lmc rainbow-mode csharp-mode browse-kill-ring dos))
-    
-    (while packages-list
-      (setq current-package (pop packages-list))
-
-      (if (not (package-installed-p current-package))
-          (progn
-            (message "installing package: %s" current-package)
-            (package-install current-package))))))
-
 (load-file "~/.emacs.d/personal/drexyia-common.el")
-(drexyia-use-proxy)
-(drexyia-emacsd-install-packages)
+
+;;(drexyia-use-proxy)
+(prelude-require-packages '(key-chord helm lmc rainbow-mode csharp-mode browse-kill-ring dos yaml-mode))
 
 ;;set ui elements
 (set-frame-font "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1") ;;use consolas font
@@ -30,6 +15,21 @@
 (global-set-key (kbd "s-b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
-(helm-mode t)
 (cua-selection-mode t)
+
+(setq prelude-whitespace nil)
+(setq prelude-flyspell nil)
+(setq prelude-guru nil)
+
+;;NOTE: prelude should automatically define these however currently this is not working on some machines
+;;      due to this i define these manually 
+(key-chord-define-global "jj" 'ace-jump-word-mode)
+(key-chord-define-global "jk" 'ace-jump-char-mode)
+(key-chord-define-global "jl" 'ace-jump-line-mode)
+(key-chord-define-global "JJ" 'prelude-switch-to-previous-buffer)
+(key-chord-define-global "uu" 'undo-tree-visualize)
+(key-chord-define-global "xx" 'execute-extended-command)
+(key-chord-define-global "yy" 'browse-kill-ring)
+(key-chord-mode +1)
+
 (server-start)
